78a0c32812ecec26a86e808260fb3e39f81eed8d
KamleshBhat654 <kamleshbhat018@gmail.com>
2025-04-21 12:38:52 +0000

    stack and heap

---
+++ 01_basics/stackandheap.js
@@ -0,0 +1,18 @@
+let name1="HELLO"
+let surname= name1
+surname="WORLD"
+console.log(name1);
+console.log(surname);
+let userOne={
+    email:"abc@gmail.com",
+    upi:"abc.upi"
+}
+let userTwo=userOne
+userTwo.email=userOne
+userTwo.email="def@gmail.com"
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
+
+
