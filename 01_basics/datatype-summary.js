584a4e64efe30dc431f4b81dcba42d3975ec16fc
KamleshBhat654 <kamleshbhat018@gmail.com>
2025-04-21 12:26:12 +0000

    datatype summary

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,8 @@
+let a=100 //Number
+let b="HI" ///String
+let id=null //null
+let name;//undefined
+let boolean=1 //boolean
+const di =Symbol('123')//Symbol
+const anotherdi=Symbol('123')
+console.log(di===anotherdi);
