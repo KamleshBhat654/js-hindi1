68c4c78954eb3400fb6a0818dcbf747365ab5a0c
KamleshBhat654 <kamleshbhat018@gmail.com>
2025-04-21 11:39:44 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,8 @@
+const accountId=123345;
+let accountName="HELLO";
+var accountPassword="12345";
+accountCity="Jaipur";
+accountName="WORLD";
+accountPassword=2222;
+accountCity="MUMBAI";
+console.table([accountId,accountName,accountPassword,accountCity])
