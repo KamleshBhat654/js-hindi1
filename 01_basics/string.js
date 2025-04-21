91a521efb11583d2d8df4ece9f6d9d21167fbd64
KamleshBhat654 <kamleshbhat018@gmail.com>
2025-04-21 12:50:09 +0000

    string

---
+++ 01_basics/string.js
@@ -0,0 +1,13 @@
+const name="KAMLESH"
+const marks=80
+console.log(`HELLO MY NAME IS ${name} AND MY MARKS ARE ${marks}`);
+const gamename=new String('HELLO')
+console.log(gamename[0])
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
+console.log(gamename.toLowerCase());
+console.log(gamename.charAt(3));
+console.log(gamename.indexOf('o'));
+
+
+
