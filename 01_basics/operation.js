a8919f8522eaa6b051effb18c928829025b7efde
KamleshBhat654 <kamleshbhat018@gmail.com>
2025-04-21 12:11:28 +0000

    operations

---
+++ 01_basics/operation.js
@@ -0,0 +1,10 @@
+let str1="HELLO"
+let str2=" KAMLESH"
+let str3=str1+str2
+console.log(str3);
+console.log(+true)
+let counter=100;
+counter++
+++counter//
+console.log(counter);//
+//console.log(def)
