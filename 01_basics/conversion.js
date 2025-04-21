6960dc7e8cb766996482ddad0a612935b520482e
KamleshBhat654 <kamleshbhat018@gmail.com>
2025-04-21 11:56:07 +0000

    Conversion

---
+++ 01_basics/conversion.js
@@ -0,0 +1,10 @@
+let score="33aaaaa";
+console.log(typeof score);
+console.log(typeof(score));
+let value=Number(score);
+console.log(typeof value);
+console.log(value);
+let a=33;
+console.log(typeof a)
+let s=String(a);
+console.log(typeof(s));
