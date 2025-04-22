8c99c75e9f346e33337e6ec83683dc38a7d849b2
KamleshBhat654 <kamleshbhat018@gmail.com>
2025-04-22 12:21:31 +0000

    IIFE

---
+++ 01_basics/IIFE.js
@@ -0,0 +1,9 @@
+(function chai(){
+    console.log(`DB CONNECTED`);
+}());
+(function aurcode(){
+    console.log(`DB CONNECTED TWO`);
+})();
+( (name)=>{
+    console.log(`DB CONNECTED TWO ${name}`);
+})("KAMLESH")
