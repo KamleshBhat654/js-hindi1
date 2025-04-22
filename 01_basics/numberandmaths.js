7b415a284f48d75ac7ee3a37d96e44c7308c7bf0
KamleshBhat654 <kamleshbhat018@gmail.com>
2025-04-22 06:06:54 +0000

    numberandmaths

---
+++ 01_basics/numberandmaths.js
@@ -0,0 +1,22 @@
+const score=400
+console.log(score);
+const balance=new Number(100)
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+const hundred=100000000
+console.log(hundred.toLocaleString());
+
+
+
+//MATHS//
+const abc=4.333
+console.log(Math.abs(abc));
+console.log(Math.round(abc));
+console.log(Math.ceil(abc));
+console.log(Math.floor(abc));
+console.log(Math.min(2,3,5,8,6,9));
+
+console.log(Math.max(2,3,5,8,6,9));
+
+
