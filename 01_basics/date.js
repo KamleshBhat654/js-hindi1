b63dfdd42dca310c1bc66483f2fe6dc28d2809f3
KamleshBhat654 <kamleshbhat018@gmail.com>
2025-04-22 06:31:30 +0000

    Date

---
+++ 01_basics/date.js
@@ -0,0 +1,14 @@
+//let myDate=new Date();
+//console.log(myDate);
+//console.log(myDate.toString());
+//console.log(myDate.toDateString());
+//console.log(myDate.toISOString());
+//console.log(myDate.toLocaleString());
+//let createDate=new Date(2025,4,10)
+//console.log(createDate.toDateString());
+let newDate=new Date()
+console.log(newDate.getDate());
+newDate.toLocaleString("default",{
+    weekday:"short",
+
+});
