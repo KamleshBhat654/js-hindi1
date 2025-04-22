add03a1a6f7bdd15c1430bf0657397408f2fdd92
KamleshBhat654 <kamleshbhat018@gmail.com>
2025-04-22 11:51:25 +0000

    arrow

--- 01_basics/arrow.js
+++ 01_basics/arrow.js
@@ -13,3 +13,16 @@ const user={
 user.welcome()
 user.username="MS"
 user.welcome()
+console.log(this);
+
+
+function chai(){
+    console.log(this)
+}
+chai()
+const addTwo=(num1,num2)=>{
+    return num1+num2
+}
+console.log(addTwo(3,5));
+const addThree=(num1,num2)=> num1*num2
+console.log(addThree(3,5));
