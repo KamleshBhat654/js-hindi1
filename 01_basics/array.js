759b5312551149b1af12bfe3d33bb91281471504
KamleshBhat654 <kamleshbhat018@gmail.com>
2025-04-22 07:01:43 +0000

    Array

---
+++ 01_basics/array.js
@@ -0,0 +1,20 @@
+//const myArr=[2,3,4,5,6,7,8,9]
+//console.log(myArr);
+//console.log(myArr[0]);
+//const car=["BMW","AUDI","HONDA"]
+//console.log(car);
+//const abc =new Array(1,2,3,4,5)
+//abc.unshift(0);
+//abc.shift();
+//console.log(abc.includes(9));
+//console.log(abc);
+//const newArr=myArr.join();
+//console.log(newArr);
+const myArr=[1,2,3,4,5,6]
+console.log("A",myArr);
+const my1=myArr.slice(1,3)
+console.log("ORIGINAL ARRAY",myArr);
+console.log("B",my1);
+const my2=myArr.splice(1,3)
+console.log("ORIGINAL ARRAY",myArr);
+console.log("C",my2);
