ccaaf4df1439ccdf1c3eb8210c038d8461e2915a
KamleshBhat654 <kamleshbhat018@gmail.com>
2025-04-23 06:49:53 +0000

    forofloop

---
+++ 01_basics/forofloop.js
@@ -0,0 +1,13 @@
+const arr=['a','e','i','o','u'];
+for (const element of arr) {
+    console.log(element);
+}
+const greeting="HELLO WORLD"
+for (const greet of greeting) {
+    console.log(`Value is ${greet}`);
+}
+const arr1=[10,20,30];
+for (const value of arr1) {
+    //value+=1; //only is let is written in for condition
+    console.log(value);
+}
