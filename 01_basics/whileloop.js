77acd7ac76c43317027397505678c4e031d8e898
KamleshBhat654 <kamleshbhat018@gmail.com>
2025-04-23 06:22:29 +0000

    whileloop

---
+++ 01_basics/whileloop.js
@@ -0,0 +1,17 @@
+let i= 0
+while (i<=10) {
+    while(i==6){
+        console.log(`Number Found`);
+        break
+    }
+    i=i+2
+    console.log(i);
+    
+}
+let Array=['Hi','How','Are','You']
+let  arr=0
+while (arr <Array.length) {
+    console.log(`Value is ${Array[arr]}`);
+    arr=arr+1
+    
+}
